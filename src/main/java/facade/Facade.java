5cadb59e592dd7e25b6c663ff0c0303c86d918b4
rasmus <rasmus@10.50.131.25>
2016-10-04 11:52:47 +0200

    rest API + changes / added methods in the facade
    Conflicts:
    	.gitignore

--- src/main/java/facade/Facade.java
+++ src/main/java/facade/Facade.java
@@ -23,127 +23,155 @@ import javax.persistence.Query;
  * @author rasmus
  */
 public class Facade {
-    
-    EntityManagerFactory emf ;
+
+    EntityManagerFactory emf;
 
     public Facade(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    
-    private EntityManager getEntityManager(){
+
+    private EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
-    
+
+    public List<Person> getPersons() {
+        EntityManager em = emf.createEntityManager();
+        List<Person> persons = null;
+        try {
+            em.getTransaction().begin();
+            persons = em.createQuery("Select p from Person p").getResultList();
+            em.getTransaction().commit();
+            return persons;
+        } finally {
+            em.close();
+        }
+    }
+
+    public Person getPersonById(int id) {
+        return new Person();
+    }
+
+    public List<Phone> getPhonesByPerson(Person p) {
+        return new ArrayList<Phone>();
+    }
+
+    public Address getAdressByPerson(Person p) {
+        return new Address();
+    }
+
+    public CityInfoNew getCityInfoByPerson(Person p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
     //Given phone number
-    public Person getPerson(String phone){
-        EntityManager em=getEntityManager();
+    public Person getPerson(String phone) {
+        EntityManager em = getEntityManager();
         Person p = new Person();
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             p = em.find(Person.class, phone); //it should have an access in the phone through the Person
             em.getTransaction().commit();
-        }finally{
+        } finally {
             em.close();
         }
-        
+
         return p;
     }
-    
-    public Company getCompany(String phoeOrCvr){
+
+    public Company getCompany(String phoeOrCvr) {
         //in this method we should search for the given parameter in first the
         //phone field, if nothing foud we should search in the cvr field and
         //return null if no company with either matching phone or cvr
-        EntityManager em=getEntityManager();
+        EntityManager em = getEntityManager();
         Company c = new Company();
-        
-        try{
+
+        try {
             em.getTransaction().begin();
             c = em.find(Company.class, phoeOrCvr); //it should have an access in the phone through the Company
             em.getTransaction().commit();
-        }finally{
+        } finally {
             em.close();
         }
-        
+
         return c;
     }
-    
-    public List<Person> getPersonsByHobby(Hobby hob){
-        
-        EntityManager em= getEntityManager();
-        try{
+
+    public List<Person> getPersonsByHobby(Hobby hob) {
+
+        EntityManager em = getEntityManager();
+        try {
             Query query = em.createQuery("SELECT p,h from Person p, Hobby h WHERE h.name= :hobby");
             query.setParameter("hobby", hob.getName());
             List<Person> persons = query.getResultList();
             return persons;
-        }finally{
+        } finally {
             em.close();
         }
-        
+
     }
-    
-    public List<Person> getPersonsByCity(String zipOrCity){
+
+    public List<Person> getPersonsByCity(String zipOrCity) {
         //the same as before, we should use one string and search in two fiels,
         //MAYBE do a check wether the string looks like a city name og a zip code,
         //and the only search in the relevent field?
-        EntityManager em= getEntityManager();
-        try{
+        EntityManager em = getEntityManager();
+        try {
             Query query;
             if (Character.isDigit(zipOrCity.charAt(0))) {
                 query = em.createQuery("SELECT p,c from Person p, CityInfo c  WHERE c.ZIP= :zip");
                 query.setParameter("zip", zipOrCity);
-            }else{
+            } else {
                 query = em.createQuery("SELECT p,c from Person p, CityInfo c  WHERE c.CITY= :city");
                 query.setParameter("city", zipOrCity);
             }
             List<Person> persons = query.getResultList();
             return persons;
-        }finally{
+        } finally {
             em.close();
         }
     }
-    
-    public int getCountOfPeopleByHobby(Hobby hob){
+
+    public int getCountOfPeopleByHobby(Hobby hob) {
         return 0;
     }
-    
-    public List<CityInfoNew> getZipCodes(){
+
+    public List<CityInfoNew> getZipCodes() {
         return new ArrayList<CityInfoNew>();
     }
-    
-    public List<Company> getCompaniesByEmpAmount(int number){
+
+    public List<Company> getCompaniesByEmpAmount(int number) {
         return new ArrayList<Company>();
     }
-    
+
     //ADD we might consider if we want to give a ie. person object, or if we will
     //just give the required parameters to create a new person object
-    public Person addPerson(Person p){
-       throw new UnsupportedOperationException("Not supported yet.");
+    public Person addPerson(Person p) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    public void addCompany(Company c){
-        
+
+    public void addCompany(Company c) {
+
     }
-    
-    public void addHobby(Hobby h){
-        
+
+    public void addHobby(Hobby h) {
+
     }
-    
-    public void addInfoEntity(InfoEntity ie){
-        
+
+    public void addInfoEntity(InfoEntity ie) {
+
     }
-    
-    public void addPhone(InfoEntity ie, String phone){
-        
+
+    public void addPhone(InfoEntity ie, String phone) {
+
     }
-    
-    public void addAddress(Address adr){
-        
+
+    public void addAddress(Address adr) {
+
     }
-    
-    public void addCityInfo(){
-        
+
+    public void addCityInfo() {
+
     }
 
 }

0660c739e33c1472660d99df60965e476ca02901
Danielh21 <danielhollmann@hotmail.com>
2016-10-05 17:29:37 +0200

    Hobby form Contiuned

--- src/main/java/rest/HobbyFacadeREST.java
+++ src/main/java/rest/HobbyFacadeREST.java
@@ -29,8 +29,8 @@ import javax.ws.rs.core.MediaType;
  * @author rasmus
  */
 @Stateless
-@Path("enitity.hobby")
-public class HobbyFacadeREST extends AbstractFacade<Hobby> {
+@Path("hobby")
+public class HobbyFacadeREST {
     
     static Facade facade = new Facade(Persistence.createEntityManagerFactory("remote"));
     static Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -39,69 +39,16 @@ public class HobbyFacadeREST extends AbstractFacade<Hobby> {
     private EntityManager em;
 
     public HobbyFacadeREST() {
-        super(Hobby.class);
     }
 
     @POST
-    @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void create(Hobby entity) {
-        super.create(entity);
-    }
-    
-    @POST
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
-    public String addCompany(String content){
+    public String addHobby(String content){
         Hobby h = gson.fromJson(content, Hobby.class);
         Hobby newHobby = facade.addHobby(h);
         return gson.toJson(newHobby);
     }
 
-    @PUT
-    @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void edit(@PathParam("id") Integer id, Hobby entity) {
-        super.edit(entity);
-    }
-
-    @DELETE
-    @Path("{id}")
-    public void remove(@PathParam("id") Integer id) {
-        super.remove(super.find(id));
-    }
-
-    @GET
-    @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Hobby find(@PathParam("id") Integer id) {
-        return super.find(id);
-    }
-
-    @GET
-    @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Hobby> findAll() {
-        return super.findAll();
-    }
-
-    @GET
-    @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Hobby> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        return super.findRange(new int[]{from, to});
-    }
-
-    @GET
-    @Path("count")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String countREST() {
-        return String.valueOf(super.count());
-    }
-
-    @Override
-    protected EntityManager getEntityManager() {
-        return em;
-    }
     
 }
--- src/main/webapp/JavaScript/JSforNewHobby.js
+++ src/main/webapp/JavaScript/JSforNewHobby.js
@@ -1,22 +1,27 @@
 
-$(function(){
-    
-    $("#hobbySubmit").on("click", function(){
-       var jsonobject;
-       
-       
-       var ajaxCall = $.ajax({
-           url: "api/",
-           type: "POST",
-           data: jsonobject
-       });
-       
-       ajaxCall.done(function(data,status){
-          console.log(data); 
-       });
-        
+$(function () {
+
+    $("#hobbySubmit").on("click", function () {
+        var object = new Object();
+        object.name = $("#hobbyName").val();
+        object.description = $("#hobbyDescription").val();
+        var jsonobject = JSON.stringify(object);
+        var ajaxCall = $.ajax({
+            url: "http://localhost:8080/CA-2/api/hobby",
+            type: "application/json",
+            data: jsonobject,
+            method: "POST"
+        });
+
+        ajaxCall.done(function (data, status) {
+            console.log(data);
+        });
+        ajaxCall.fail(function (xhr, status, error) {
+            console.log("Error!!! " + xhr.responseText);
+        });
+
     });
-    
-}); 
+
+});
 
 
